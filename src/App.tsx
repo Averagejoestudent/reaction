a051bdac8a77c71f7cab6f0a80b9556783ba973e
Averagejoestudent <zoimmoiz@gmail.com>
2025-07-09 23:26:11 +0500

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='mb-4'>
+      Hello world
+    </h1>
+    <p>Muhammad Moiz Ahmed</p>
+    </>
+  )
+}
+
+export default App
